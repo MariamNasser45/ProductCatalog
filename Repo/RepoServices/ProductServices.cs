dab39c4cd53aee683fc619bb14f8a63750b6ab85
Mariam Nasser <96596668+MariamNasser45@users.noreply.github.com>
2023-07-17 18:11:56 +0300

    solve issue of duration

--- Repo/RepoServices/ProductServices.cs
+++ Repo/RepoServices/ProductServices.cs
@@ -33,7 +33,7 @@ namespace ProductCatalog.Repo.RepoServices
             //dont't need to convert 
             //return Context.Products.Where(c => Convert.ToInt32(DateTime.Now.Day- c.startDate.Day) < c.duration).ToList(); 
 
-            return Context.Products.Where(c => (DateTime.Now.Day - c.startDate.Day) < c.duration).ToList();
+            return Context.Products.Where(c => (DateTime.Now.Day - c.startDate.Day) < (c.startDate.AddDays(c.duration).Day)).ToList();
         }
 
 
